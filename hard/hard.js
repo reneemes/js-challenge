6d8b45a1105afc75a928c8dd020d123ef6abc41d
Renee Messersmith <reneemessersmith@Administrators-MacBook-Pro.local>
2025-11-25 12:53:22 -0500

    Feat: completed hard challenge

--- hard/hard.js
+++ hard/hard.js
@@ -1,4 +1,18 @@
+function createPerson(name, ssn) {
+  const PII = {
+    name: name,
+    SSN: ssn
+  }
 
+  return {
+    getName: function() {
+      return PII.name;
+    },
+    getSSN: function() {
+      return undefined;
+    }
+  }
+}
 
 const patient2 = createPerson('John', '123-45-6789');
 console.log(patient2.names);
